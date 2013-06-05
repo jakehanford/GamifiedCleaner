dac29fe100b9331caeca977e5f6195889ad99273
Jack Hanford <jackhanford@gmail.com>
2013-06-05 16:26:30 -0700

    I don't even know where to start I have added so much^_^

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130531010645) do
+ActiveRecord::Schema.define(:version => 20130605221115) do
+
+  create_table "checks", :force => true do |t|
+    t.boolean  "kitchen"
+    t.boolean  "living"
+    t.boolean  "bunk"
+    t.boolean  "bathroom"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "homes", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -25,21 +34,19 @@ ActiveRecord::Schema.define(:version => 20130531010645) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.text     "name"
+    t.string   "email"
+    t.string   "encrypted_password"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count"
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
