27cab17fa6681b88a3940587ea5cee6ddc8f936b
Jack Hanford <jackhanford@gmail.com>
2013-06-09 15:51:18 -0700

    Lots of new styling

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
-  attr_accessible :taskcomplete
+  attr_accessible :taskcomplete, :name
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :name
+
+  has_many :tasks
+
   # attr_accessible :title, :body :name,
 end
